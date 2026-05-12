a4446bcd114a6170bda19e1b1899a95dde3f0117
Udeagha Chukwubuikem Xavier <danonymousuchiha@gmail.com>
2026-05-12 10:45:15 +0100

    A little more refactoring

--- AutoLoads/Cache.gd
+++ AutoLoads/Cache.gd
@@ -10,9 +10,6 @@ func _ready() -> void:
 	render_info = {
 		# ENEMY
 		Enums.ENTITY_TYPES.NORMAL_ENEMY: ImageGenerator.create_render_profile(Enums.SHAPE_TYPES.CIRCLE, 8, Color.RED, Constants.TILE_SIZE * 0.3, true),
-		# TOWERS
-		Enums.ENTITY_TYPES.BASE: ImageGenerator.create_render_profile(Enums.SHAPE_TYPES.SQUARE, 12, Color.BLUE, Constants.TILE_SIZE * 2, true),
-		Enums.ENTITY_TYPES.TURRET: ImageGenerator.create_render_profile(Enums.SHAPE_TYPES.SQUARE, 12, Color.RED, Constants.TILE_SIZE, true),
 		# PROJECTILES
 		Enums.ENTITY_TYPES.BULLET: ImageGenerator.create_render_profile(Enums.SHAPE_TYPES.CIRCLE, 0, Color.RED, Constants.TILE_SIZE * 0.1, false),
 		# UI
@@ -20,9 +17,7 @@ func _ready() -> void:
 	}
 	textures_dict = {
 		Enums.ENTITY_TYPES.NORMAL_ENEMY: await ImageGenerator.generate_texture_for(render_info[Enums.ENTITY_TYPES.NORMAL_ENEMY]),
-		Enums.ENTITY_TYPES.BASE: await ImageGenerator.generate_texture_for(render_info[Enums.ENTITY_TYPES.BASE]),
 		Enums.ENTITY_TYPES.BULLET: await  ImageGenerator.generate_texture_for(render_info[Enums.ENTITY_TYPES.BULLET]),
-		Enums.ENTITY_TYPES.TURRET: await  ImageGenerator.generate_texture_for(render_info[Enums.ENTITY_TYPES.TURRET]),
 		Enums.ENTITY_TYPES.CELL_HOVER_OVERLAY: await ImageGenerator.generate_texture_for(render_info[Enums.ENTITY_TYPES.CELL_HOVER_OVERLAY])
 	}
 	
--- AutoLoads/Stats.gd
+++ AutoLoads/Stats.gd
@@ -4,12 +4,7 @@ extends Node
 	Enums.ENTITY_TYPES.NORMAL_ENEMY: Misc.create_meele_data_for(Enums.LEVELS.HIGH, Enums.LEVELS.HIGH)
 }
 
-@onready var projectile_weapon_data: Dictionary[Enums.ENTITY_TYPES, ProjectileWeaponData] = {
-	Enums.ENTITY_TYPES.TURRET: Misc.create_projectile_data_for(Enums.LEVELS.HIGH, Enums.LEVELS.LOW, Enums.LEVELS.HIGH, [Enums.ALIGNMENTS.ENEMY])
-}
 
 @onready var health_data: Dictionary[Enums.ENTITY_TYPES, HealthData] = {
-	Enums.ENTITY_TYPES.BASE: Misc.create_health_data(100),
 	Enums.ENTITY_TYPES.NORMAL_ENEMY: Misc.create_enemy_health_data_for(Enums.LEVELS.LOW),
-	Enums.ENTITY_TYPES.TURRET: Misc.create_tower_health_data_for(Enums.LEVELS.LOW)
 }
